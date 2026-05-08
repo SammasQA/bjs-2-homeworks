95117985a98379fe4e5f0dd5280a4f37590697f8
Sergey Mashinin <smashinin@mail.ru>
2026-05-08 14:45:06 +0300

    Решение задач: квадратное уравнение и ипотечный калькулятор

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,20 @@
 "use strict"
+
+// Задача 1: квадратное уравнение
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
-}
+  let d = b ** 2 - 4 * a * c; // дискриминант
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (d < 0) {
+    return arr; // пустой массив
+  } else if (d === 0) {
+    let root = -b / (2 * a);
+    arr.push(root);
+  } else {
+    let sqrtD = Math.sqrt(d);
+    let root1 = (-b + sqrtD) / (2 * a);
+    let root2 = (-b - sqrtD) / (2 * a);
+    arr.push(root1, root2);
+  }
+  return arr;
 }
